be0593ec113ff7b0ba738f26c8fc9dcb5e023f98
Loki807 <santhanloki28@gmail.com>
2025-11-12 15:06:23 +0530

    loki

---
+++ KaappaanPlus.Infrastructure/Identity/SignalRAlertNotifier.cs
@@ -0,0 +1,27 @@
+﻿using KaappaanPlus.Application.Contracts.Communication;
+using KaappaanPlus.WebApi.Hubs;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaappaanPlus.Infrastructure.Identity
+{
+    public class SignalRAlertNotifier : IAlertNotifier
+    {
+        private readonly IHubContext<AlertHub> _hubContext;
+
+        public SignalRAlertNotifier(IHubContext<AlertHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        public async Task SendAlertAsync(object payload, string[] roles)
+        {
+            foreach (var role in roles)
+                await _hubContext.Clients.Group(role).SendAsync("ReceiveAlert", payload);
+        }
+    }
+}
