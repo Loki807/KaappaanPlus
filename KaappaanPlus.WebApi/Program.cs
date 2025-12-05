e49892cecba7d32d20c9a6b567b00b9c10e399a8
fathimanitha03 <fathimanitha025@gmail.com>
2025-12-05 15:57:56 +0530

    co

--- KaappaanPlus.WebApi/Program.cs
+++ KaappaanPlus.WebApi/Program.cs
@@ -17,12 +17,11 @@ namespace KaappaanPlus.WebApi
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Application Layers
             builder.Services.AddApplicationServices();
             builder.Services.AddPersistence(builder.Configuration);
             builder.Services.AddInfrastructureServices(builder.Configuration);
 
-            // JWT Authentication
+            // JWT
             var jwt = builder.Configuration.GetSection("JwtSettings");
             builder.Services
                 .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -35,61 +34,55 @@ namespace KaappaanPlus.WebApi
                         ValidateIssuerSigningKey = true,
                         ValidIssuer = jwt["Issuer"],
                         ValidAudience = jwt["Audience"],
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt["Key"]!))
+                        IssuerSigningKey =
+                            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt["Key"]!))
                     };
                 });
 
-            // ⭐ FIXED CORS (Azure + Netlify)
+            // ⭐ FIXED CORS
             builder.Services.AddCors(options =>
             {
                 options.AddPolicy("AllowAll", policy =>
-                    policy.WithOrigins(
+                    policy
+                        .WithOrigins(
                             "https://kaappaan.netlify.app",
                             "http://localhost:4200"
                         )
                         .AllowAnyHeader()
                         .AllowAnyMethod()
-                        .AllowCredentials()
                 );
             });
 
             builder.Services.AddAuthorization();
-
-            // Controllers + Swagger
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
-            // SignalR
             builder.Services.AddSignalR();
 
             var app = builder.Build();
 
-            // Swagger
             app.UseSwagger();
             app.UseSwaggerUI();
 
-            // Global Error Handler
-            app.UseMiddleware<ErrorHandlingMiddleware>();
-
-            // ⭐ Enable CORS middleware
+            // ⭐ MUST BE FIRST MIDDLEWARE
             app.UseCors("AllowAll");
 
             app.UseAuthentication();
             app.UseAuthorization();
 
-            // Seed Database
+            // Error handling middleware LAST
+            app.UseMiddleware<ErrorHandlingMiddleware>();
+
             using (var scope = app.Services.CreateScope())
             {
                 var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
                 await SeedDataRunner.RunAllAsync(db);
             }
 
-            // Routes
             app.MapHub<AlertHub>("/alertHub");
             app.MapControllers();
 
             app.Run();
         }
     }
-} //new cors
+}
